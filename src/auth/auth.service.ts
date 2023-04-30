8f65f13bc98779f88bdbb39a02004a8041b663ee
ClockWize171 <jupiterminhtin998@gmail.com>
2023-04-30 21:52:09 +0630

    OTW to configure Authentication and passport

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDto) {
+    const { name, email, password } = signUpDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = this.jwtService.sign({ id: user._id });
+    return { token };
+  }
+}
